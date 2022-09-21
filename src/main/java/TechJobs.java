30c437c7f28fffa4964cc8bdcb8262fc349ab4b0
dhalamicek <110735781+dhalamicek@users.noreply.github.com>
2022-09-20 21:26:53 -0500

    Implemented printJobs

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +112,37 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
+    //if there are jobs, then it should print and keep looping until there are no jobs in the list, then stop
+    //if there are no jobs, then it should print "No results"
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+
+        if (someJobs.size() == 0) {
+            System.out.println("No results");
+            return;
+        } else {
+            for (HashMap<String, String> job : someJobs) {
+
+                String breakLine = "*****";
+                System.out.println(breakLine);
+
+                    for (Map.Entry<String, String> jobKey : job.entrySet()) {
+
+                        System.out.println(jobKey.getKey() + ": " + jobKey.getValue());
+                    }
+                    System.out.println(breakLine+"\n");
+                }
+            }
+
+        }
     }
-}
+
+
+
+
